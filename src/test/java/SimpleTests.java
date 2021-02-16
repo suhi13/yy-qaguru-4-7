b491d652db8f77bb8e8d341e22db9bd07d4bace9
svasenkov <aanher@gmail.com>
2021-02-16 21:25:45 +0300

    some tag for jenkins parametrized tests

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -1,14 +1,15 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleTests {
     @Test
+    @Tag("positive")
     void successTest() {
         assertEquals(true, true);
 //        assertTrue(true);
@@ -20,6 +21,7 @@ public class SimpleTests {
     }
 
     @Test
+    @Tag("positive")
     void successWithHamcrestTest() {
         assertThat(true, is(true));
     }
@@ -30,6 +32,7 @@ public class SimpleTests {
     }
 
     @Test
+    @Tag("positive")
     @DisplayName("Some positive test")
     void successWithHamcrestAndStepsTest() {
         step("Assert that true is true", () ->
